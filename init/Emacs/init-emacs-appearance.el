0fd5b09ec4cafe0016c34bc95926c96e5175ab7c
FITMYFIT <richardliu1987@163.com>
2020-10-21 10:54:44 +0800

    disable aggressive-fill-paragraph-mode in latex-mode.

--- init/Emacs/init-emacs-appearance.el
+++ init/Emacs/init-emacs-appearance.el
@@ -167,7 +167,7 @@
 (use-package aggressive-fill-paragraph
   :ensure t
   :defer t
-  :init (add-hook 'text-mode-hook #'aggressive-fill-paragraph-mode)
+  ;; :init (add-hook 'text-mode-hook #'aggressive-fill-paragraph-mode)
   :config (add-to-list 'afp-fill-keys ?。) ; support for Chinese sentence end.
   (add-to-list 'afp-fill-comments-only-mode-list 'emacs-lisp-mode)
   (add-to-list 'afp-fill-comments-only-mode-list 'lisp-mode)
